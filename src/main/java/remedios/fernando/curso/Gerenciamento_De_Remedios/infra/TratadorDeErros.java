62c60c8383ce54f016b6c83f709eddaa251e15e9
Fernando Rodrigues de Santana <fernandorodrigusantana@gmail.com>
2024-08-06 17:14:24 -0300

    feat: treating exceptions 404 not found

---
+++ src/main/java/remedios/fernando/curso/Gerenciamento_De_Remedios/infra/TratadorDeErros.java
@@ -0,0 +1,16 @@
+package remedios.fernando.curso.Gerenciamento_De_Remedios.infra;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
+import org.springframework.web.bind.annotation.RestControllerAdvice;
+
+import jakarta.persistence.EntityNotFoundException;
+
+@RestControllerAdvice
+public class TratadorDeErros {
+	
+	@ExceptionHandler(EntityNotFoundException.class)
+	public ResponseEntity<?> tratador404() {
+		return ResponseEntity.notFound().build();
+	}
+}
