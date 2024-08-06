67fcbeea035c974ab8b616289b229baa45b338b9
Fernando Rodrigues de Santana <fernandorodrigusantana@gmail.com>
2024-08-06 17:26:51 -0300

    feat: trating the fields in registration

--- src/main/java/remedios/fernando/curso/Gerenciamento_De_Remedios/infra/TratadorDeErros.java
+++ src/main/java/remedios/fernando/curso/Gerenciamento_De_Remedios/infra/TratadorDeErros.java
@@ -1,6 +1,8 @@
 package remedios.fernando.curso.Gerenciamento_De_Remedios.infra;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.validation.FieldError;
+import org.springframework.web.bind.MethodArgumentNotValidException;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -12,5 +14,18 @@ public class TratadorDeErros {
 	@ExceptionHandler(EntityNotFoundException.class)
 	public ResponseEntity<?> tratador404() {
 		return ResponseEntity.notFound().build();
+	}
+	
+	@ExceptionHandler(MethodArgumentNotValidException.class)
+	public ResponseEntity<?> tratador400(MethodArgumentNotValidException ex) {
+		var erros = ex.getFieldErrors();		
+		
+		return ResponseEntity.badRequest().body(erros.stream().map(DadosErros::new).toList());
+	}
+	
+	public record DadosErros(String message, String field) {
+		public DadosErros(FieldError erro) {
+			this(erro.getField(), erro.getDefaultMessage());
+		}
 	}
 }
